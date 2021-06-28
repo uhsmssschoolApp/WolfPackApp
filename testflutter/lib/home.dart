5f13bfb9b292f7127268abb6b3e08b371bf17e2b
Sarina Li <sarinajin.li@gmail.com>
2021-06-28 18:52:07 -0400

    webview mwah

--- testflutter/lib/home.dart
+++ testflutter/lib/home.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'consts.dart';
 
 class home extends StatefulWidget {
@@ -264,114 +265,136 @@ class _HomeState extends State<home> {
                 style: TextStyle(
                     fontFamily: "SFBold", fontSize: 20.0, color: grey),
               ),
-              // color: Colors.black,
             ),
             Center(
               child: Column(
                 children: [
-                  Container(
-                    // wrap in gesture thing to add on tap
-                    margin: EdgeInsets.only(top: 12.0),
-                    width: screenWidth * 0.9,
-                    height: 100,
-                    // color: Colors.blue,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: homeCorners,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 7,
-                            offset: Offset(0, 5)),
-                      ],
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(left: 16.0, right: 16.0),
-                          alignment: Alignment.center,
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            color: maroon,
-                            borderRadius: homeCorners,
-                          ),
-                          child: Container(
-                            child: Image.asset(
-                              "assets/cheering-megaphone_1f4e3.png",
-                              height: 35,
-                              width: 35,
+                  InkWell(
+                    // the ANNOUNCEMENTS box
+                    onTap: () {
+                      // write code later
+                      // print("hi");
+                    },
+                    child: Container(
+                      // wrap in gesture thing to add on tap
+                      margin: EdgeInsets.only(top: 12.0),
+                      width: screenWidth * 0.9,
+                      height: 100,
+                      // color: Colors.blue,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: homeCorners,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 7,
+                              offset: Offset(0, 5)),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 16.0, right: 16.0),
+                            alignment: Alignment.center,
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              color: maroon,
+                              borderRadius: homeCorners,
+                            ),
+                            child: Container(
+                              child: Image.asset(
+                                "assets/cheering-megaphone_1f4e3.png",
+                                height: 35,
+                                width: 35,
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 6,
-                          child: Container(
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    alignment: Alignment.bottomLeft,
+                          Expanded(
+                            flex: 6,
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      alignment: Alignment.bottomLeft,
+                                      child: Text(
+                                        "Announcements",
+                                        style: TextStyle(
+                                          fontFamily: "SFBold",
+                                          color: const Color(0xFF252525),
+                                          fontSize: 18.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 10.0,
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        "Get all your latest updates here!",
+                                        style: TextStyle(
+                                          fontFamily: "SF",
+                                          color: const Color(0xFF858585),
+                                          fontSize: 14.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(top: 24.0),
+                                    alignment: Alignment.center,
+                                    height: 20,
+                                    width: 20,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: maroon,
+                                    ),
                                     child: Text(
-                                      "Announcements",
+                                      ">",
                                       style: TextStyle(
                                         fontFamily: "SFBold",
-                                        color: const Color(0xFF252525),
-                                        fontSize: 18.0,
+                                        fontSize: 14.0,
+                                        color: Colors.white,
                                       ),
                                     ),
                                   ),
-                                ),
-                                Container(
-                                  height: 10.0,
-                                ),
-                                Expanded(
-                                  flex: 1,
-                                  child: Container(
-                                    alignment: Alignment.topLeft,
+                                  Container(
+                                    alignment: Alignment.bottomRight,
+                                    margin: const EdgeInsets.only(
+                                        top: 16.0, right: 20.0),
+                                    height: 30,
+                                    width: 50,
+                                    // color: Colors.black,
                                     child: Text(
-                                      "Get all your latest updates here!",
+                                      "06-28",
                                       style: TextStyle(
                                         fontFamily: "SF",
+                                        fontSize: 12.0,
                                         color: const Color(0xFF858585),
-                                        fontSize: 14.0,
                                       ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            child: Column(
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.only(top: 24.0),
-                                  alignment: Alignment.center,
-                                  height: 20,
-                                  width: 20,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: maroon,
-                                  ),
-                                  child: Text(
-                                    ">",
-                                    style: TextStyle(
-                                      fontFamily: "SFBold",
-                                      fontSize: 14.0,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                   Container(
@@ -386,7 +409,11 @@ class _HomeState extends State<home> {
                   Container(
                     margin: EdgeInsets.only(top: 12.0, bottom: 12.0),
                     width: screenWidth * 0.9,
-                    height: 100,
+                    height: 400,
+                    child: WebView(
+                      initialUrl: "https://www.youtube.com/",
+                      javascriptMode: JavascriptMode.unrestricted,
+                    ),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: homeCorners,
