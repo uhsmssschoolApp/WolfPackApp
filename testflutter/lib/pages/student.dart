e4be7e4f023e55e3cb156bb73f7f3cff4379e820
dourian <75771131+dourian@users.noreply.github.com>
2021-07-07 00:05:18 -0400

    firebase android

--- testflutter/lib/pages/student.dart
+++ testflutter/lib/pages/student.dart
@@ -14,8 +14,6 @@ class student extends StatefulWidget {
   _studentState createState() => _studentState();
 }
 
-
-
 List<Widget> screenDisplays = [
   feed(),
   Center(
@@ -23,9 +21,8 @@ List<Widget> screenDisplays = [
   ),
 ];
 
-
-int activePageNumber = 0; //the page that the user is on
-// Widget curView = webviewDisplay();
+int activePageNumber = 1; //the page that the user is on
+Widget curView = webviewDisplay();
 
 class _studentState extends State<student> {
   // @override
@@ -68,6 +65,10 @@ class _studentState extends State<student> {
                   }),
             ),
           ),
+          // Container(
+          //   height: 40,
+          //   color: Colors.black,
+          // ),
           Expanded(
             flex: 7,
             child: screenDisplays[curDisplay],
@@ -76,6 +77,80 @@ class _studentState extends State<student> {
       ),
     );
   }
+}
+
+void webPageChanger(int page) {
+  activePageNumber = page;
+}
+
+Widget webviewDisplay() {
+  if (activePageNumber == 1) {
+    return WebView(
+      initialUrl: twitterURL,
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  } else if (activePageNumber == 2) {
+    print("inti2");
+    return WebView(
+      initialUrl: uhsURL,
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  } else {
+    print("uh oh");
+    return Container();
+  }
+}
+
+Widget feed() {
+  return Column(
+    children: [
+      Container(
+        height: 40,
+        // color: Colors.black,
+        child: Row(
+          children: [
+            Container(
+              height: 30,
+              width: 90,
+              // color: Colors.black,
+              child: OutlinedButton(
+                  onPressed: () {
+                    webPageChanger(1);
+                    curView = webviewDisplay();
+                  },
+                  child: Text(
+                    "@YRDSB",
+                    style: TextStyle(
+                      fontFamily: "SFBold",
+                      color: Colors.indigo,
+                    ),
+                  )),
+              margin: const EdgeInsets.only(left: 8),
+            ),
+            Container(
+              height: 30,
+              width: 90,
+              child: OutlinedButton(
+                  onPressed: () {
+                    webPageChanger(2);
+                    curView = webviewDisplay();
+                  },
+                  child: Text("@UHS",
+                      style: TextStyle(
+                        fontFamily: "SFBold",
+                        color: Colors.indigo,
+                      ))),
+              margin: const EdgeInsets.only(left: 8),
+            ),
+          ],
+        ),
+      ),
+      Expanded(
+        flex: 1,
+        child: curView,
+      ),
+    ],
+  );
 }
 
 //int i = 0;
