d1335bf015a78442e6fa1306de10f9404bbeb6cb
dourian <75771131+dourian@users.noreply.github.com>
2021-08-03 22:08:56 -0400

    going to revert

--- testflutter/lib/main.dart
+++ testflutter/lib/main.dart
@@ -9,9 +9,12 @@ import 'pages/links.dart';
 import 'pages/student.dart';
 import 'themes/mythemes.dart';
 import 'pages/more.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  //await Firebase.initializeApp();
+void main() /*async*/ {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
