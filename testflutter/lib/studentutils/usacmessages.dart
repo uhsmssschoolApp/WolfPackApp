c0644d50d29203d085c25de6931ba6f97088944f
Sarina Li <sarinajin.li@gmail.com>
2021-08-13 23:46:34 -0400

    errir

--- testflutter/lib/studentutils/usacmessages.dart
+++ testflutter/lib/studentutils/usacmessages.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:testflutter/constructors/appbar.dart';
+import 'package:testflutter/firestore.dart';
 
 class usacFeed extends StatefulWidget {
-  const usacFeed({ Key? key }) : super(key: key);
+  const usacFeed({Key? key}) : super(key: key);
 
   @override
   _usacFeedState createState() => _usacFeedState();
@@ -13,6 +14,24 @@ class _usacFeedState extends State<usacFeed> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: mainAppBar("USAC Messages"),
+      body: ListView.builder(
+        // reverse: true,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 200,
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(masterStreamList[index].title),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: masterStreamList.length,
+      ),
     );
   }
 }
