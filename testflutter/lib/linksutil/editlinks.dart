2f2ae4c8b3c51253af4477dea0da786ae38f32da
Sarina Li <sarinajin.li@gmail.com>
2021-12-15 18:52:22 -0500

    went to eat

--- testflutter/lib/linksutil/editlinks.dart
+++ testflutter/lib/linksutil/editlinks.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:testflutter/constants/consts.dart';
 import 'package:testflutter/constructors/appbar.dart';
 
-List<Widget> myLinks = List.empty();
+// List<Widget> myLinks = List.empty();
+List<String> myLinks = ["hi", "yo", "no"];
 
-class EditLinks extends StatelessWidget {
+class EditLinks extends StatefulWidget {
   const EditLinks({Key? key}) : super(key: key);
 
-  // TEMP SOLUTION: CREATE A LIST THAT DOESNT PERSIST
-  // see dismissable: https://api.flutter.dev/flutter/widgets/Dismissible-class.html (FOR LINKS TO DELETE)
+  @override
+  State<EditLinks> createState() => _EditLinksState();
+}
 
+class _EditLinksState extends State<EditLinks> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +22,39 @@ class EditLinks extends StatelessWidget {
           // refer to docs: https://docs.flutter.dev/cookbook/persistence/reading-writing-files
           // store the links to persist data
           children: [
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: myLinks.length,
-                itemBuilder: (context, index) {
-                  return myLinks[index];
-                }),
+            Container(
+              margin: usualMargin,
+              child: const ListTile(
+                title: Text(
+                  "My Custom Links",
+                  style: TextStyle(
+                    fontFamily: "SFBold",
+                    fontSize: 17.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: myLinks.length,
+                  itemBuilder: (context, index) {
+                    final item = myLinks[index];
+                    return Dismissible(
+                        key: Key(item),
+                        background: Container(color: maroon),
+                        onDismissed: (direction) {
+                          // Remove the item from the data source.
+
+                          // Then show a snackbar.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('link deleted!')));
+                        },
+                        child: ListTile(
+                          title: Text(item),
+                        ));
+                  }),
+            ),
             Container(
               margin: usualMargin,
               child: Text("hi"),
